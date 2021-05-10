ac76ca1d28c05e4b1b0d74dd704491397bc53978
1729087928@qq.com <1729087928@qq.com>
2021-05-10 21:15:12 +0800

    四则运算

--- Cshap/Program.cs
+++ Cshap/Program.cs
@@ -6,8 +6,18 @@ namespace Cshap
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            int a = 1, b = 5;
+            int sum, difference, product;
+            double merchant;
+            sum = a + b;
+            difference = a - b;
+            product = a * b;
+            merchant = a / b;
+            Console.WriteLine(sum);
+            Console.WriteLine(difference);
+            Console.WriteLine(product);
+            Console.WriteLine(merchant);
+          
         }
     }
 }
